1e067171d2f6ed952872d2ae4d24bbe0344fc752
zhanglianjie1 <zhanglianjie1@vipkid.com.cn>
2020-09-04 14:43:38 +0800

    remove _score

--- src/main/java/org/plugin/PayloadSimilarity.java
+++ src/main/java/org/plugin/PayloadSimilarity.java
@@ -1,6 +1,7 @@
 package org.plugin;
 
 import org.apache.lucene.analysis.payloads.PayloadHelper;
+import org.apache.lucene.index.FieldInvertState;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.util.BytesRef;
 
@@ -13,7 +14,31 @@ public class PayloadSimilarity extends ClassicSimilarity {
             return 1;
         }
     }
+    @Override
+    public float coord(int overlap, int maxOverlap) {
+        return 1;
+    }
 
+    @Override
+    public float queryNorm(float sumOfSquaredWeights) {
+        return 1.0f;
+    }
+
+    @Override
+    public float tf(float freq) {
+        return 1.0f;
+    }
+
+
+    @Override
+    public float idf(long docFreq, long docCount) {
+        return 1.0f;
+    }
+
+    @Override
+    public float lengthNorm(FieldInvertState state) {
+        return 1.0f;
+    }
     @Override
     public String toString() {
         return "PayloadSimilarity";
